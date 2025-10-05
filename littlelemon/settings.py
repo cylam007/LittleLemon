f458834e42d599b15bcba77f44920e39440ff920
Cy LAM <chungyeunglam@gmail.com>
2025-10-06 01:14:19 +0800

    rest-api

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'Restaurant',
 ]
 
@@ -137,3 +139,16 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+}
+
+# Tell djoser to use 'username' as the unique identifier for users
+DJOSER = {'USER_ID_FIELD': 'username'}
